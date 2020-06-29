ea2de25f2e442697b498953b1bac0e71b3b7b93d
LiuShaow <441653848@qq.com>
2020-06-29 12:23:23 +0800

    提交day01_git2

---
+++ day01_git2/src/cn/jiyun/test/Test.java
@@ -0,0 +1,10 @@
+package cn.jiyun.test;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println("HelloGit!");
+	System.out.println("HelloGit!");
+	System.out.println("HelloGit!");
+	System.out.println("HelloGit!");
+}
+}
